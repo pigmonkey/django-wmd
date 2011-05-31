f004d7d1ffcabdc7f627922d4ffd44dbff8cceaf
Pig Monkey <pm@pig-monkey.com>
2011-05-31 14:05:59 -0700

    going with a slugified project name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'Django WMD',
+    name = 'django-wmd',
     version = '0.1',
     description = 'A resuable Django app for a Markdown WMD editor widget.',
     long_description = open('README.md').read(),
